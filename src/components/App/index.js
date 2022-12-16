08e5c8d169ca86ce47da41aa57bcf3c3c66adc26
rdesitter <rdesitter@gmail.com>
2022-12-16 12:02:53 +0100

    feat: private route + check is user is logged

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,37 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router';
+import Admin from '../../pages/Admin';
 import Home from '../../pages/Home';
 import Login from '../../pages/Login';
+import { setUser } from '../../reducers/userSlice';
 
 import '../../style/style.scss';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // If user is in localstorage -> log user
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem('token');
+
+    if(token) {
+      dispatch(setUser(user));
+    }
+  })
+  const logged = useSelector((state) => state.user.logged);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="/connexion" element={ <Login /> } />
+        
+        {/* Private routes */}
+        {logged && (
+          <Route path="/dashboard" element={ <Admin /> } />
+        )}
       </Routes>
     </div>
   );
